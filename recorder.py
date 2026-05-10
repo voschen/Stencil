74e705085a17b277ec0aa4fba29a64da2af64198
Voschen <giovannichebli777@gmail.com>
2026-05-10 22:05:19 +0300

    added communication between python and js + click detection in the terminal

--- recorder.py
+++ recorder.py
@@ -1,14 +1,33 @@
 from playwright.sync_api import sync_playwright, Playwright
 
 
+
+def handle_click(data):
+    print("clicked:", data)
+
+
 def run(playwright: Playwright):
-    chromium = playwright.chromium # or "firefox" or "webkit".
+    chromium = playwright.chromium
     browser = chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
+    
+    context.expose_function("sendToPython", handle_click)
+
+    page.add_init_script(script="""
+        document.addEventListener('click', (e) => {
+            console.log('CLICK CAUGHT')
+            window.sendToPython(e.target.tagName)
+        }, true)
+    """, )
+    
     page.goto("http://wikipedia.com")
-    # other actions...
-    # browser.close()
+    page.evaluate("window.sendToPython('test')")
+    page.pause()
+
+
+
 
 with sync_playwright() as playwright:
+    
     run(playwright)
-    input("Press Enter to close...")
